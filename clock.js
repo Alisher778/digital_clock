0d1ecc66beda1b104c42396ebf91f6b37716d6a4
Alisher778 <internet-docs@mail.ru>
2016-10-10 19:48:02 -0400

    created HTML5 scaffold

--- clock.js
+++ clock.js
@@ -6,31 +6,22 @@ function timeRun(){
    var mil = currentTime.getMilliseconds();
    var diem = 'am';
 
-   if (h>12){ //Declaring if the time AM or PM
-     h=h-12; 
-     diem = 'pm';
-   }
-   if (h<10){//Add 0 before time if less than 10. Not 9 instead 09
-    h = "0"+h;
-   }
-   if (m<10){//this is for minites
-    m = "0"+m;
-   }
-   if (s<10){
-    s = "0"+s;
-   }
-   if (mil<10){
-    mil = "00"+mil;
-   }
-   if (mil<100){
-    mil = "0"+mil;
-   }
-   if (mil==1000){
-    mil = 000;
-   }
+//Declaring if the time AM or PM
+   if (h>12){ h=h-12; diem = 'pm';}
+   //Add 0 before time if less than 10. Not 9 instead 09
+   if (h<10){ h = "0"+h; }
 
-   var full = h+':'+m+':'+s+':'+mil+diem;
-   var display = document.getElementById('list');
+   if (m<10){ m = "0"+m; }//this is for minites
+
+   if (s<10){ s = "0"+s; }//this is for seconds
+
+   //Milleseconds correction by adding 000s in it
+   if (mil<10){ mil = "00"+mil; }
+   if (mil<100){ mil = "0"+mil; }
+   if (mil==1000){ mil = 000; }
+
+   var full = h+':'+m+':'+s+':'+mil+diem;//displaying full time
+   var display = document.getElementById('clock');
    display.innerHTML = full;
    setTimeout(timeRun, 100);
 }
